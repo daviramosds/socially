0e1ca429591718929210e61c701d47c812bc295a
Davi R <contact@davirds.dev>
2025-02-13 21:54:22 -0300

    add: theme toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button"
+import ModeToggle from "@/components/ModeToggle";
 
 export default function Home() {
   return (
     <div className="m-4">
+      <ModeToggle />
       <SignedOut>
         <SignInButton mode="modal">
           <Button variant="default">Sign In</Button>
